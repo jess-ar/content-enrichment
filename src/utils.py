01d8251b9c8e43ea40aaeb4110fe776619a6f803
tursdlc <ana89.sdlc@gmail.com>
2024-07-18 15:43:56 +0200

    added user inputs

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,31 @@
-print('hello world')
+def user_query_input():
+        try:
+            user_input = input("Ingresa tu consulta")
+            if not user_input.isalpha():
+                raise ValueError("Debe contener solo caracteres alfabéticos. Por favor, intenta de nuevo.")
+            return user_input
+        except ValueError as e:
+            print(e)
+
+def user_gpt_input():
+        try:
+            user_input = input("¿Quieres mejorar el texto?").strip().lower()
+            if user_input not in ['yes', 'no']:
+                raise ValueError("La respuesta debe ser 'yes' o 'no'. Por favor, intenta de nuevo.")
+            return user_input == 'yes'
+        except ValueError as e:
+            print(e)
+
+def user_translate_input():
+        try:
+            user_input = input("¿Quieres traducir el texto?").strip().lower()
+            if user_input not in ['yes', 'no']:
+                raise ValueError("La respuesta debe ser 'yes' o 'no'. Por favor, intenta de nuevo.")
+            return user_input == 'yes'
+        except ValueError as e:
+            print(e)
+
+
+print(user_query_input())
+print(user_gpt_input())
+print(user_translate_input())
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,1 +1,20 @@
-print("test")
+"""
+import pytest
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+from utils import user_query_input
+
+
+def test_user_query_input(user_input, expected):
+    assert user_query_input(user_input.isalpha()) == expected
+"""
+"""
+def test_user_gpt_input(user_input, expected):
+    if user_input in ['yes', 'no']:
+        assert user_gpt_input("¿Quieres mejorar el texto?") == expected
+    else:
+        with pytest.raises(ValueError):
+            user_gpt_input("¿Quieres mejorar el texto? (yes/no): ")
+
+"""
