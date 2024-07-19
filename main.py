b4b1542fd4832288e15c2681f6f10bb7714313e8
Seda07 <gevorgyanseda0@gmail.com>
2024-07-19 09:59:30 +0200

    add main

--- main.py
+++ main.py
@@ -7,3 +7,20 @@ def main():
 main()
 
 
+"""from .src.Gpt import GptService, TextImprover
+def main():
+    ti = TextImprover
+    gpt = GptService(textimprover=ti)
+
+    while True:
+        i = input('Write some text or quit to exit')
+
+        if i == 'quit':
+            return
+
+        result = gpt.textimprover(i)
+        print(result)
+
+if name == '__main':
+    main()"""
+
