b519ab6b5a88544b79411666a7e90447c9458adc
CIBA <sanchezmariabelen01@gmail.com>
2024-07-21 18:58:57 +0200

    add generator in Content Enrichment

--- src/Content_enrichment.py
+++ src/Content_enrichment.py
@@ -2,12 +2,14 @@ from src.Scraping import Scraping
 from src.Gpt import TextImprover, GptService
 from src.Translator import Translator
 from src.utils import get_user_input, get_user_confirmation
+from src.Generator import Generator
 
 class ContentEnrichment:
     def __init__(self):
         self.text_improver = TextImprover()
         self.gpt_service = GptService(self.text_improver)
         self.translator = Translator()
+        self.generator = Generator()
 
     def run(self):
         while True:
@@ -36,6 +38,20 @@ class ContentEnrichment:
                         print(f"Texto Traducido:\n{translated_content}")
                     except Exception as e:
                         print(f"Error al traducir el texto: {e}")
+
+                    if get_user_confirmation("generar archivo PDF del texto traducido"):
+                        file_name = "document.pdf"
+                        change_name = get_user_input("¿Deseas cambiar el nombre del archivo? (s/n): ").strip().lower()
+                        if change_name == "s":
+                            file_name = get_user_input("Introduce el nuevo nombre del archivo(con extensión .pdf): ").strip()
+
+                        try:
+                            pdf = Generator()
+                            pdf.generate_pdf(title, translated_content, file_name)
+                            print(f"PDF generado con éxito: {file_name}")
+                        except Exception as e:
+                            print(f"Error al generar el PDF: {e}")
+
 
             else:
                 print("No se encontró contenido para la búsqueda.")
